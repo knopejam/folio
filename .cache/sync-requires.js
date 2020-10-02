3ac90c337a484940a2cae6916ff160fb7957a2e9
knopejam <knopejam@gmail.com>
2020-10-01 21:24:37 -0400

    changed primary and secondary colors

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/joanneiveson/Desktop/portfolio/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/joanneiveson/Desktop/portfolio/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/joanneiveson/Desktop/portfolio/src/pages/index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/lindsayiveson/Desktop/portfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/lindsayiveson/Desktop/portfolio/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/lindsayiveson/Desktop/portfolio/src/pages/index.js")))
 }
 
